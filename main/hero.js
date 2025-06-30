370c21197ec2dce2503db02de691f13224fe4eaa
Marina Al <alyohin@gmail.com>
2025-06-30 15:05:13 +0500

    fonts

--- main/hero.js
+++ main/hero.js
@@ -1,9 +1,14 @@
 const textEl = document.querySelector('[wb-data="text"]');
-const splitText = new SplitText(textEl, { type: "lines" });
-gsap.set(textEl, { autoAlpha: 1 });
-gsap.from(splitText.lines, {
-    autoAlpha: 0,
-    lineHeight: "3em",
-    duration: 1.5,
-    clearProps: "lineHeight"
-});
+
+if (textEl) {
+    document.fonts.ready.then(() => {
+        const splitText = new SplitText(textEl, { type: 'lines' });
+        gsap.set(textEl, { autoAlpha: 1 });
+        gsap.from(splitText.lines, {
+            autoAlpha: 0,
+            lineHeight: '3em',
+            duration: 1.5,
+            clearProps: 'lineHeight'
+        });
+    });
+}
