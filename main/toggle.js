a2e834a35b8fe45c915705fd302295a44cbac0f6
Marina Al <alyohin@gmail.com>
2025-07-08 20:20:45 +0500

    upd

--- main/toggle.js
+++ main/toggle.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const toggleButton = document.querySelector('[data-theme-toggle-button]');
   const buttonText = toggleButton.querySelector('.lights-on');
   const body = document.body;
+  const bottomGradient = document.querySelector('.bottom-gradient');
   
   let isDarkMode = false;
   
@@ -13,28 +14,31 @@ document.addEventListener('DOMContentLoaded', function() {
       const currentColor = getComputedStyle(body).color;
       
       const wasInDarkMode = body.classList.contains('theme-dark');
-      
-      body.classList.remove('theme-light', 'theme-dark');
-      body.classList.add(wasInDarkMode ? 'theme-light' : 'theme-dark');
-      
-      const newBg = getComputedStyle(body).backgroundColor;
-      const newColor = getComputedStyle(body).color;
+      const newBgColor = wasInDarkMode ? '#f4f4f4' : '#14161a';
       
       body.classList.remove('theme-light', 'theme-dark');
       body.style.backgroundColor = currentBg;
       body.style.color = currentColor;
       
-      gsap.to(body, {
-          backgroundColor: newBg,
-          color: newColor,
-          duration: 0.5,
+      const tl = gsap.timeline({
           ease: "power1.out",
           onComplete: function() {
               body.style.backgroundColor = '';
               body.style.color = '';
+              bottomGradient.style.backgroundImage = '';
               body.classList.add(wasInDarkMode ? 'theme-light' : 'theme-dark');
           }
       });
+      
+      tl.to(body, {
+          backgroundColor: newBgColor,
+          color: wasInDarkMode ? '#14161a' : '#f4f4f4',
+          duration: 0.3
+      }, 0)
+      .to(bottomGradient, {
+          backgroundImage: `linear-gradient(0deg, ${newBgColor}, transparent)`,
+          duration: 0.3
+      }, 0);
       
       isDarkMode = !wasInDarkMode;
       buttonText.textContent = isDarkMode ? 'lights on' : 'lights off';
